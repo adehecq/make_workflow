be21a156f2b4ddef90ea22109343ea7d3dbb10d8
Amaury Dehecq <amaury.dehecq@jpl.nasa.gov>
2019-03-14 14:17:46 -0700

    Modified python tools to use a class rather than functions. By default will generate a temporary file rather than requiring to name one. Main targets are automatically added with any new append. Add a "display" function to print the Makefile on the screen.

--- make_workflow.py
+++ make_workflow.py
@@ -6,6 +6,148 @@
 # 03/2019
 
 import os, sys
+from tempfile import NamedTemporaryFile, mkstemp
+
+class Workflow():
+
+    def __init__(self, filename=None, title=None):
+        """
+        Used to initialize the makefile. Will generate a main function for the makefile. Optionally, will display a title string at beginning of execution.
+        filename: str, path to the makefile (Default is None, i.e. generate a temporary file)
+        title: string to display at the beginning of the execution
+        """
+
+        # Create and open temporary file
+        if filename==None:
+            tmpf = NamedTemporaryFile()
+            filename = tmpf.name
+            f = tmpf.file
+            self.tmpf = tmpf   # Keep otherwise file will be deleted
+            self.filename = tmpf.name
+            
+        # Other open chosen file 
+        else:
+            f = open(filename, 'w+b')  # writing and reading
+            self.filename = filename
+            
+        # Write header
+        f.write(".PHONY: MAIN\n\n")
+
+        # Write MAIN line, without title
+        if title is None:
+            f.write("MAIN: \n\n")
+
+        # with title
+        else:
+            f.write("MAIN: pre-build \n\n")
+            f.write("pre-build:\n\t@printf '%s\\n'\n\n" %title)
+
+        # save
+        f.flush()
+        self.f = f
+
+        
+    def append(self, cmds, inputs, outputs, title=None):
+        """
+        Add a new list of commands to the Makefile with given outputs and inputs and display a title string at beginning of excution.
+        Commands can be a single command or a list of commands.
+        """
+        
+        # Convert potential lists into string with space separator
+        outputs = check_args_inout(outputs)
+        inputs = check_args_inout(inputs)
+
+        # Write target:deps line
+        self.f.write("\n%s : %s\n" %(outputs, inputs))
+
+        # Add command for title
+        if title is not None:
+            self.f.write("\t@printf '%s\\n'\n" %escape_char(title))
+
+        # Add all commands
+        cmds = check_args_cmd(cmds)
+
+        for cmd in cmds:
+            # Escape special characters
+            #cmd = escape_char(cmd)
+
+            # print command with + symbol and green color
+            self.f.write("\t-@echo '[32m+%s[0m'\n" %cmd)
+
+            # command to be run
+            self.f.write("\t@%s\n" %cmd)                       
+
+        ## Need to update the MAIN function to add new outputs ##
+
+        # Read the text and replace MAIN
+        filetext = ''
+        self.f.seek(0)
+        for line in self.f:
+            if line[:4]=='MAIN':
+                line = ' '.join([line.rstrip(),outputs,'\n'])
+                filetext += line
+            else:
+                filetext += line
+
+        # Write to file
+        self.f.seek(0)
+        self.f.write(filetext)
+        self.f.flush()
+
+        
+    def display(self):
+        """
+        Print the current makefile to the screen.
+        """
+        self.f.seek(0)
+        filetext = self.f.read()
+        print(filetext)
+        #os.system('cat %s' %self.filename)
+        
+
+    def append_secondary(self, second):
+        """
+        Add files to be considered as secondary, i.e., the files will not be recreated if deleted.
+        """
+        self.f.write(".SECONDARY : %s\n" %second)
+        self.f.flush()
+
+
+    def run(self, njobs=1, dryrun=False, debug=False, force=False, other_args=None):
+        """
+        Run the makefile with njobs parallel jobs.
+        njobs: int, number of parallel jobs to run.
+        dryrun: bool, set to True to print the commands without running them.
+        debug: bool, set to True to run in debug mode.
+        force: bool, set to True to ignore errors and keep the worflow running.
+        other_args: str, any other argument to pass to make
+        """
+        cmd = "make -f %s" %self.filename
+
+        # Check njobs option and append
+        if isinstance(njobs,int):
+            if njobs>1:
+                cmd.append(' -j %i' %njobs)
+        else:
+            print("ERROR: njobs must be of type int")
+            sys.exit()
+
+        # Append other options
+        if dryrun!=False:
+            cmd += ' -n'
+        if debug!=False:
+            cmd += ' -d'
+        if force!=False:
+            cmd += ' -i'
+
+        # Allow any other arguments to be passed
+        if other_args is not None:
+            cmd += ' ' + other_args 
+
+        # Run make
+        os.system(cmd)
+
+        
 
 def check_args_inout(args):
     """
@@ -17,7 +159,8 @@ def check_args_inout(args):
         args = [ os.path.normpath(arg) for arg in args ]
         args = ' '.join(args)
     elif isinstance(args,str):
-        args = os.path.normpath(args)
+        if len(args)>0:   # exclude empty string
+            args = os.path.normpath(args)
     else:
         print("ERROR: argument must be iterable (list, tuple, array) or string")
         print(args)
@@ -52,103 +195,3 @@ def escape_char(string):
     string = string.replace('"','\\"')  # escape "
     string = string.replace("\n","\\n")  # escape \n
     return string
-    
-    
-def makefile_init(filename, targets, title=None):
-    """
-    Usage: init_makefile(filename,targets,[title])
-    Used to initialize the makefile. Will generate a main function and main targets for the makefile. Optionally, will display a title string at beginning of execution.
-    """
-
-    # Open file and write initial line
-    f = open(filename, 'w')
-    f.write(".PHONY: MAIN\n\n")
-
-    # if targets is iterable, create a string separated by spaces
-    targets = check_args_inout(targets)
-
-    # Write MAIN line
-    if title is None:
-        f.write("MAIN: %s\n\n" %targets)
-    else:
-        f.write("MAIN: pre-build %s\n\n" %targets)
-        f.write("pre-build:\n\t@printf '%s\\n'\n\n" %title)
-    f.close()
-
-    
-def makefile_append(filename, cmds, inputs, outputs, title=None):
-    """
-    Usage: makefile_append filename title target dependencies cmd1 cmd2...
-    Will add a new list of commands to the makefile 'filename' with given outputs and inputs and display a title string at beginning of excution.
-    commands can be a single command or a list of commands.
-    """
-    
-    f = open(filename, 'a')
-
-    # Convert potential lists into string with space separator
-    outputs = check_args_inout(outputs)
-    inputs = check_args_inout(inputs)
-
-    f.write("\n%s : %s\n" %(outputs, inputs))
-    
-    if title is not None:
-        f.write("\t@printf '%s\\n'\n" %escape_char(title))
-    
-    cmds = check_args_cmd(cmds)
-
-    for cmd in cmds:
-        # Escape special characters
-        #cmd = escape_char(cmd)
-
-        # print command with + symbol and green color
-        f.write("\t-@echo '[32m+%s[0m'\n" %cmd)
-
-        # command to be run
-        f.write("\t@%s\n" %cmd)                       
-        
-    f.close()
-
-    
-def makefile_secondary(filename, second):
-    """
-    Usage makefile_secondary filename second 
-    Add files to be considered as secondary, i.e., the files will not be recreated if deleted.
-    """
-    f = open(filename, 'a')
-    f.write(".SECONDARY : %s\n" %second)
-    f.close()
-
-    
-def makefile_run(filename, njobs=1, dryrun=False, debug=False, force=False, other_args=None):
-    """
-    Run the makefile with njobs parallel jobs.
-    njobs: int, number of parallel jobs to run.
-    dryrun: bool, set to True to print the commands without running them.
-    debug: bool, set to True to run in debug mode.
-    force: bool, set to True to ignore errors and keep the worflow running.
-    other_args: str, any other argument to pass to make
-    """
-    cmd = "make -f %s" %filename
-
-    # Check njobs option and append
-    if isinstance(njobs,int):
-        if njobs>1:
-            cmd.append(' -j %i' %njobs)
-    else:
-        print("ERROR: njobs must be of type int")
-        sys.exit()
-
-    # Append other options
-    if dryrun!=False:
-        cmd += ' -n'
-    if debug!=False:
-        cmd += ' -d'
-    if force!=False:
-        cmd += ' -i'
-
-    # Allow any other arguments to be passed
-    if other_args is not None:
-        cmd += ' ' + other_args 
-
-    # Run make
-    os.system(cmd)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Test the makefile_utils for Python
+# Test the make_worflow tools for Python
 # Amaury Dehecq
 # 03/2019
 
@@ -10,25 +10,16 @@ import os, sys
 
 
 # Initialize workflow
-mw.makefile_init('Makefile',targets=['hello2','hello3'],title="*** Test flow ***")
-
+wf = mw.Workflow(title="*** Test flow ***")
 
 # Create some text file
-hello1_ins = []
-hello1_outs = ["hello1"]
-hello1_cmds = ["echo foo > "+hello1_outs[0] ]
-mw.makefile_append('Makefile',hello1_cmds, hello1_ins, hello1_outs, title='\n** Hello1 **')
+wf.append("echo foo > hello1", "", "hello1", title='\n** Hello1 **')
 
 # Then, export
-hello2_outs = ["hello2"]
-hello2_cmds = ["sed 's/foo/faa/' "+hello1_outs[0]+" > "+hello2_outs[0]]
-mw.makefile_append('Makefile', hello2_cmds, hello1_outs, hello2_outs, title='\n** Hello2 **')
+wf.append("sed 's/foo/faa/' hello1 > hello2", "hello1", "hello2", title='\n** Hello2 **')
 
 # Another target, do not depend on either hello1 & 2
-hello3_ins = []
-hello3_outs = ["hello3"]
-hello3_cmds = ["echo bar > "+hello3_outs[0]]
-mw.makefile_append('Makefile',hello3_cmds, hello3_ins, hello3_outs, title='\n** Hello3 **')
+wf.append("echo bar > hello3", "", "hello3", title='\n** Hello3 **')
 
 # Finaly, create and run workflow
-mw.makefile_run('Makefile',njobs=1)
+wf.run(njobs=1)
